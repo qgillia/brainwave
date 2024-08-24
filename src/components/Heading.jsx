c3fd7c7d7d545893822acf3f8743039cf05b4408
qgillia <qgillia@gmail.com>
2024-08-24 03:56:46 -0400

    implement benefits section

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+const Heading = ({ classNames, title }) => {
+  return (
+    <div className={`${classNames} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
+      {title && <h2 className="h2">{title}</h2>}
+    </div>
+  );
+};
+
+export default Heading;
